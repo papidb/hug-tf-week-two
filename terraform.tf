e922a5d18793c12d6b31dfb3fb68728881e6bb48
Daniel Benjamin <benjamindaniel706@gmail.com>
2026-08-28 22:41:34 +0100

    chore: add the s3 backend

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,17 @@
 terraform {
+  required_version = ">= 1.10.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.92"
     }
   }
-  required_version = ">=1.2"
+
+  backend "s3" {
+    key          = "hug-tf-week-two/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
